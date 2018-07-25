d2566b67f3eac602b013f2b4e51d569865025beb
VooDooS <thevoodoos@gmail.com>
2018-07-25 15:38:47 +0200

    Cleaning and commenting

--- lp2tex.py
+++ lp2tex.py
@@ -13,10 +13,6 @@ print(lpCode)
 infer_r = r"(.*?):-(.*?)\."
 infer_s = r"\\infer{\g<1>}{\g<2>}"
 
-# Regex for identifiers
-ident_r = r"([a-z]\w*)"
-ident_s = r"\\text{\g<1>}"
-
 # Regex for idents
 ident_r = r"(^|\n|\r|\t|\W)([a-z]\w*)"
 ident_s = r"\g<1>\\texttt{\g<2>}"
@@ -25,17 +21,19 @@ ident_s = r"\g<1>\\texttt{\g<2>}"
 back_r = r"\\(?!texttt)"
 back_s = r"\\backslash"
 
+# First we add \texttt around identifiers
 step1 = re.sub(
     ident_r, ident_s, lpCode, 0, re.MULTILINE)
 
+# Then we replace \ by \backslash (except for \texttt of course)
 step2 = re.sub(
     back_r, back_s, step1, 0, re.MULTILINE)
 
+# Then we substitute various symbols by their latex version
 step3 = step2.replace(
     "=>", "\\Rightarrow").replace(r" ", "~")
 
-
-# Moving to inference rules
+# Eventually we replace :- clauses by inference rules
 result = re.sub(infer_r, infer_s, step3, 0, re.MULTILINE)
 
 print(result)
